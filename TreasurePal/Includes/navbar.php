20e0f237a1805a3d85e49fd98f7e9b051ea603a0
millerz50 <135332424+millerz50@users.noreply.github.com>
2024-11-08 09:09:49 +0200

    Add files via upload
    This is  a  property listing  project  with php  dynamic  website.Any one is  free to list a property

---
+++ TreasurePal/Includes/navbar.php
@@ -0,0 +1,73 @@
+
+<?php
+define ("title" ,"TreasurePal");
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title><?php  echo  title ;?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="../assets/css/templatemo-plot-listing.css">
+    <link rel="stylesheet" href="../assets/css/animated.css">
+    <link rel="stylesheet" href="../assets/css/owl.css">
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+    <div class="row">
+      <div class="col-12">
+          <nav class="main-nav">
+              <!-- ***** Logo Start ***** -->
+              <a href="index.php" class="company">
+                 TreasurePal
+              </a>
+              <!-- ***** Logo End ***** -->
+              <!-- ***** Menu Start ***** -->
+              <ul class="nav">
+                  <li><a href="index.php" class="active">Home</a></li>
+                  <li><a href="category.php">Category</a></li>
+                  <li><a href="listing.php">Listing</a></li>
+                  <li><a href="contact.php">Contact Us</a></li>
+                  <li>
+                      <div class="main-white-button">
+                          <a href="#"><i class="fa fa-plus"></i> Add Your Listing</a>
+                      </div>
+                  </li>
+              </ul>
+              <a class='menu-trigger'>
+                  <span>Menu</span>
+              </a>
+              <!-- ***** Menu End ***** -->
+          </nav>
+      </div>
+  </div>
+  </body>
