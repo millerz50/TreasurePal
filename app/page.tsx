994aebc3e90fe6801b910d1f508413319187794d
Millerz50 <johwanisi1@gmail.com>
2025-11-23 03:21:57 +0200

    Update app/page.tsx with Home component and Navbar integration

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen bg-base-200">
-      <Navbar user={user} />
+      <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-12 py-8">
         <Hero />
         <PropertyList />
