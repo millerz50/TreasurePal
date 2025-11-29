3291d73ea5be1f2a161ffe799061979a72b4e081
Millerz50 <johwanisi1@gmail.com>
2025-11-29 17:05:01 +0200

    fixing things ui and ux mastering(fixing the the footer icons svg)*5

--- components/landing/footer/Footer.tsx
+++ components/landing/footer/Footer.tsx
@@ -228,6 +228,7 @@ export default function Footer() {
               aria-live="polite"></p>
 
             <div className="flex items-center gap-3 mt-2">
+              {/* FACEBOOK ICON */}
               <a
                 className="w-10 h-10 rounded-full border flex items-center justify-center tp-social-hoverable"
                 href="#"
@@ -238,9 +239,11 @@ export default function Footer() {
                   viewBox="0 0 24 24"
                   fill="currentColor"
                   aria-hidden="true">
-                  <path d="M22 12..." />
+                  <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 5 3.66 9.13 8.44 9.88v-6.99H8.08V12h2.36V9.8c0-2.33 1.39-3.62 3.52-3.62 1.02 0 2.09.18 2.09.18v2.3h-1.18c-1.16 0-1.52.72-1.52 1.46V12h2.58l-.41 2.89h-2.17v6.99C18.34 21.13 22 17 22 12z" />
                 </svg>
               </a>
+
+              {/* TWITTER / X ICON */}
               <a
                 className="w-10 h-10 rounded-full border flex items-center justify-center tp-social-hoverable"
                 href="#"
@@ -251,9 +254,11 @@ export default function Footer() {
                   viewBox="0 0 24 24"
                   fill="currentColor"
                   aria-hidden="true">
-                  <path d="M22 5..." />
+                  <path d="M22.46 6c-.77.35-1.6.58-2.46.69a4.29 4.29 0 00 1.88-2.37 8.59 8.59 0 01-2.72 1.04 4.27 4.27 0 00-7.3 3.89A12.1 12.1 0 013 4.8a4.27 4.27 0 001.32 5.7 4.22 4.22 0 01-1.93-.54v.05a4.28 4.28 0 003.42 4.19 4.28 4.28 0 01-1.92.07 4.28 4.28 0 003.99 2.97A8.58 8.58 0 012 19.54 12.1 12.1 0 008.29 21c7.55 0 11.68-6.26 11.68-11.68 0-.18-.01-.35-.02-.53A8.35 8.35 0 0022.46 6z" />
                 </svg>
               </a>
+
+              {/* INSTAGRAM ICON */}
               <a
                 className="w-10 h-10 rounded-full border flex items-center justify-center tp-social-hoverable"
                 href="#"
@@ -264,7 +269,7 @@ export default function Footer() {
                   viewBox="0 0 24 24"
                   fill="currentColor"
                   aria-hidden="true">
-                  <path d="M7 2..." />
+                  <path d="M7 2C4.24 2 2 4.24 2 7v10c0 2.76 2.24 5 5 5h10c2.76 0 5-2.24 5-5V7c0-2.76-2.24-5-5-5H7zm10 2a3 3 0 013 3v10a3 3 0 01-3 3H7a3 3 0 01-3-3V7a3 3 0 013-3h10zm-5 3a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6zm4.5-.25a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z" />
                 </svg>
               </a>
             </div>
